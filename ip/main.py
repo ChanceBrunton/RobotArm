8657884a2bae29ad50cb1ca8235ad9856b7058bf
chance_brunton <ntesla@nmsu.edu>
2017-01-30 12:47:40 -0700

    fixed paths in code for new directory structure

--- ip/main.py
+++ ip/main.py
@@ -9,7 +9,7 @@ import ImageProcessing as ip
 #pi.takePicture('test.jpg');
 
 testImage = 'test3.png'
-img = cv2.imread('input/'+testImage)
+img = cv2.imread('ip/input/'+testImage)
 
 if img is None:
 	raise Exception("Error while loading the image")
@@ -18,26 +18,26 @@ array = np.asarray(img);
 
 ### CONVERT TO CHROMATIC
 ip.rgbToChromatic(array)
-cv2.imwrite('output/chromatic.jpg', array)
+cv2.imwrite('ip/output/chromatic.jpg', array)
 
 ### CONVERT TO GRAY
 img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-cv2.imwrite('output/gray.jpg', img)
+cv2.imwrite('ip/output/gray.jpg', img)
 
 ### THRESHOLD
 #blur = cv2.GaussianBlur(img, (5,5), 0)
 #th2 = cv2.threshold(blur, 0, 244, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 thr = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-cv2.imwrite('output/thresh.jpg', thr)
+cv2.imwrite('ip/output/thresh.jpg', thr)
 
 ### MORPHOLOGICAL TRANSFORMATION
 kernal = np.ones((4,4),np.uint8)
 morph = cv2.morphologyEx(thr,cv2.MORPH_OPEN,kernal)
-cv2.imwrite('output/morph.jpg', morph)
+cv2.imwrite('ip/output/morph.jpg', morph)
 
 ### FIND CONTOURS
 contours = ip.findContours(morph)
 
 # fill contours
 morph = ip.fillContours(morph,contours)
-cv2.imwrite('output/contours.jpg',morph)
+cv2.imwrite('ip/output/contours.jpg',morph)
